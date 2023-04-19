97cd76200fbfc201d00cf36ad1580d336721e602
h-wakaba <h-wakabayashi@jpx.co.jp>
2023-04-19 19:29:06 +0900

    テストを実装 #24

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -25,18 +25,55 @@ test_that("get_info", {
 })
 
 
-# get_sections ------------------------------------------------------------
-test_that("get_sections", {
-  expect_error(get_sections(), NA)
-})
-
-
 # get_daily_quotes --------------------------------------------------------
 test_that("get_daily_quotes", {
   expect_error(get_daily_quotes(code = "86970"), NA)
   expect_error(get_daily_quotes(date = "20220715"), NA)
   expect_error(get_daily_quotes(code = "86970", date = "20220715"), NA)
   expect_error(get_daily_quotes(code = "86970", from = "20220701", to = "20220715"), NA)
+})
+
+
+# get_prices_am -----------------------------------------------------------
+test_that("get_prices_am", {
+  expect_error(get_prices_am(), NA)
+  expect_error(get_prices_am(code = "86970"), NA)
+})
+
+
+# get_trades_spec ---------------------------------------------------------
+test_that("get_trades_spec", {
+  expect_error(get_trades_spec(section = "TSEPrime"), NA)
+  expect_error(get_trades_spec(from = "20220101", to = "20220630"), NA)
+  expect_error(get_trades_spec(section = "TSEPrime", from = "20220101", to = "20220630"), NA)
+  expect_error(get_trades_spec(from = "20220101"), NA)
+  expect_error(get_trades_spec(to = "20081231"), NA)
+  expect_error(get_trades_spec(section = "TSEPrime", from = "20220101"), NA)
+  expect_error(get_trades_spec(section = "TSEPrime", to = "20220630"), NA)
+})
+
+
+# get_weekly_margin_interest ---------------------------------------------------------------
+test_that("get_weekly_margin_interest", {
+  expect_error(get_weekly_margin_interest(code = "86970"), NA)
+  expect_error(get_weekly_margin_interest(date = "20220701"), NA)
+  expect_error(get_weekly_margin_interest(code = "86970", from = "20220101", to = "20220630"), NA)
+})
+
+
+# get_short_selling ---------------------------------------------------------------
+test_that("get_short_selling", {
+  expect_error(get_short_selling(sector33code = "0050"), NA)
+  expect_error(get_short_selling(date = "20220701"), NA)
+  expect_error(get_short_selling(sector33code = "0050", from = "20220101", to = "20220630"), NA)
+})
+
+
+# get_breakdown ---------------------------------------------------------------
+test_that("get_breakdown", {
+  expect_error(get_breakdown(code = "86970"), NA)
+  expect_error(get_breakdown(date = "20220701"), NA)
+  expect_error(get_breakdown(code = "86970", from = "20220101", to = "20220630"), NA)
 })
 
 
@@ -49,19 +86,6 @@ test_that("get_topix", {
 })
 
 
-# get_trades_spec ---------------------------------------------------------
-test_that("get_trades_spec", {
-  expect_error(get_trades_spec(), NA)
-  expect_error(get_trades_spec(section = "TSEPrime"), NA)
-  expect_error(get_trades_spec(from = "20220101", to = "20220630"), NA)
-  expect_error(get_trades_spec(section = "TSEPrime", from = "20220101", to = "20220630"), NA)
-  expect_error(get_trades_spec(from = "20220101"), NA)
-  expect_error(get_trades_spec(to = "20220630"), NA)
-  expect_error(get_trades_spec(section = "TSEPrime", from = "20220101"), NA)
-  expect_error(get_trades_spec(section = "TSEPrime", to = "20220630"), NA)
-})
-
-
 # get_financial_statements ------------------------------------------------
 test_that("get_financial_statements", {
   expect_error(get_financial_statements(code = "86970"), NA)
@@ -70,7 +94,21 @@ test_that("get_financial_statements", {
 })
 
 
+# get_financial_dividend ------------------------------------------------
+test_that("get_financial_dividend", {
+  expect_error(get_financial_dividend(code = "86970"), NA)
+  expect_error(get_financial_dividend(date = "20220701"), NA)
+  expect_error(get_financial_dividend(code = "86970", from = "20220101", to = "20220630"), NA)
+})
+
+
 # get_financial_announcement ----------------------------------------------
 test_that("get_financial_announcement", {
   expect_error(get_financial_announcement(), NA)
+})
+
+
+# get_index_option ----------------------------------------------
+test_that("get_index_option", {
+  expect_error(get_index_option(date = "20220701"), NA)
 })
