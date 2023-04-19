4dbc7aefbda481adb2e82cc83a1bcc13027fc578
h-wakaba <h-wakabayashi@jpx.co.jp>
2023-04-19 19:20:48 +0900

    user-agentにRのバージョンを含める #24

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,8 @@ default_user_agent <- function() {
     libcurl = curl::curl_version()$version,
     `r-curl` = as.character(utils::packageVersion("curl")),
     httr = as.character(utils::packageVersion("httr")),
-    JQuantsR = as.character(utils::packageVersion("JQuantsR"))
+    JQuantsR = as.character(utils::packageVersion("JQuantsR")),
+    R = stringr::str_extract(R.version.string, "[0-9]+\\.[0-9]+\\.[0-9]+")
   )
   default_user_agent <- paste0(names(versions), "/", versions, collapse = " ")
   return(default_user_agent)
